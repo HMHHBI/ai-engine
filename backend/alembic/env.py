95bc22728b08c75ed3b0bc6e91d63684affba150
HMHHBI <hmhhbi@gmail.com>
2026-05-06 15:30:35 +0500

    fix: env.py and set the online migrations

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -11,6 +11,7 @@ from app.core.config import settings
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
+config.set_main_option("sqlalchemy.url", settings.DATABASE_URL)
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
@@ -41,7 +42,7 @@ def run_migrations_offline() -> None:
     script output.
 
     """
-    url = config.get_main_option("sqlalchemy.url", settings.DATABASE_URL)
+    url = config.get_main_option("sqlalchemy.url")
     context.configure(
         url=url,
         target_metadata=target_metadata,
